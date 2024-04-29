771ad80fd9779f996c6aad461fa2b7d72faadfa0
François L.P. <me@francois-lp.dev>
2024-04-29 19:41:40 +0200

    feat: added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import NoPage from "./pages/NoPage.jsx";
 
 export default function App() {
   return (
     <>
-      <h1>App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/NoPage.jsx
@@ -0,0 +1,3 @@
+export default function NoPage() {
+  return <h1>Aucune page trouvée!</h1>;
+}
