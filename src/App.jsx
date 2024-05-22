7505d3b49be1e048667a1cc167cf587b014745bf
François L.P. <me@francois-lp.dev>
2024-05-22 18:40:26 +0200

    feat: added a global context for concerts data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { createContext, useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Faq from "./pages/Faq.jsx";
 import Partners from "./pages/Partners.jsx";
 import AllConcerts from "./pages/AllConcerts.jsx";
 import NoPage from "./pages/NoPage.jsx";
+import { getConcertsURL } from "./utils.js";
 import "./App.css";
 
+export const ConcertsContext = createContext(null);
+
+async function fetchConcertsData() {
+  const response = await fetch(getConcertsURL());
+  const json = await response.json();
+  return json;
+}
+
 export default function App() {
+  const [concertsData, setConcertsData] = useState(null);
+  useEffect(() => {
+    (async () => {
+      const data = await fetchConcertsData();
+      setConcertsData(data);
+    })();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
@@ -14,7 +32,14 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="/faq" element={<Faq />} />
           <Route path="/partenaires" element={<Partners />} />
-          <Route path="/programmation" element={<AllConcerts />} />
+          <Route
+            path="/programmation"
+            element={
+              <ConcertsContext.Provider value={concertsData}>
+                <AllConcerts />
+              </ConcertsContext.Provider>
+            }
+          />
           <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
--- src/pages/AllConcerts.jsx
+++ src/pages/AllConcerts.jsx
@@ -1,28 +1,23 @@
+import { ConcertsContext } from "../App.jsx";
 import Header from "../components/Header/Header.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 import ConcertCard from "../components/ConcertCard/ConcertCard.jsx";
 import Separator from "../components/Separator.jsx";
 import FilterButton from "../components/FilterButton/FilterButton.jsx";
-import { getConcertsURL } from "../utils.js";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import calendarIcon from "../assets/calendar.svg";
 import locationIcon from "../assets/location.svg";
 
-async function fetchConcerts() {
-  const response = await fetch(getConcertsURL());
-  const json = await response.json();
-  return json;
-}
-
 export default function AllConcerts() {
   const [concertCards, setConcertCards] = useState(null);
+  const concerts = useContext(ConcertsContext);
 
   useEffect(() => {
-    (async () => {
-      const data = await fetchConcerts();
+    if (concerts) {
+      console.log("ye");
       setConcertCards(
-        data.map((object) => (
+        concerts.map((object) => (
           <ConcertCard
             key={object.id}
             imageSource={object.acf.cover_image}
@@ -33,8 +28,8 @@ export default function AllConcerts() {
           />
         ))
       );
-    })();
-  }, []);
+    }
+  }, [concerts]);
 
   return (
     <>
